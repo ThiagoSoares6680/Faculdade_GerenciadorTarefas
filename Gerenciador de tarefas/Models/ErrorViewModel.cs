73da0141c9071d1d48f89a932061fde7be2ee64b
thiago <thiago.98soares6680@hotmail.com>
2023-08-26 18:16:34 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciador de tarefas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerenciador_de_tarefas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
